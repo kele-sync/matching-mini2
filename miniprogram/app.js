dde48aab12ef8e434299f67d9627ff1ab465f961
kele <chj.ymh@gmail.com>
2020-01-06 10:01:54 +0800

    文件命名格式化

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -10,7 +10,7 @@ App({
           wx.getUserInfo({
             success: res => {
               // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo 
+              this.globalData.userInfo = res.userInfo;
               // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
               // 所以此处加入 callback 以防止这种情况
               if (this.userInfoReadyCallback) {
@@ -20,20 +20,20 @@ App({
           })
         }
       }
-    }) 
+    })
     this.getUserId()
   },
-  getUserId(){
-    let self=this;
+  getUserId() {
+    let self = this;
     wx.cloud.callFunction({
-      name:'login',
-      success:res=>{
-        self.globalData.userId=res.result.openid;  
-      }, 
+      name: 'login',
+      success: res => {
+        self.globalData.userId = res.result.openid;
+      },
     })
   },
   globalData: {
     userInfo: null,
-    userId:undefined
+    userId: undefined
   }
 })
--- miniprogram/pages/home/home.js
+++ miniprogram/pages/home/home.js
@@ -10,12 +10,12 @@ Page({
     canIUse: wx.canIUse('button.open-type.getUserInfo')
   },
   //事件处理函数
-  bindViewTap: function() {
+  bindViewTap: function () {
     wx.navigateTo({
       url: '../logs/logs'
     })
   },
-  onChange:function({detail:active}){
+  onChange: function ({ detail: active }) {
     wx.redirectTo({
       url: `../${active}/${active}`
     })
@@ -26,7 +26,7 @@ Page({
         userInfo: app.globalData.userInfo,
         hasUserInfo: true
       })
-    } else if (this.data.canIUse){
+    } else if (this.data.canIUse) {
       // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
       // 所以此处加入 callback 以防止这种情况
       app.userInfoReadyCallback = res => {
@@ -48,12 +48,5 @@ Page({
       })
     }
   },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
-  }
+
 })
